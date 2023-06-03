53141e8ccd8026c131d3e7864a07dcf4a17b5485
Ayush <ayush4002gupta@gmail.com>
2023-06-04 03:49:15 +0530

    new commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-
+require('dotenv').config()
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -11,8 +11,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 var title="";
-mongoose.connect("mongodb+srv://Ayush:<password>@cluster0.fohsg.mongodb.net/<Database>", {useNewUrlParser: true});
-
+mongoose
+  .connect(
+    `mongodb+srv://Ayush:${process.env.MONGO_PASS}@cluster0.fohsg.mongodb.net/${process.env.DB_NAME}`,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
 const postSchema = {
 
     title: String, 
@@ -41,7 +46,8 @@ const postSchema = {
 
 
    app.get("/",function(req,res){
-    Postmodel.find({}, function(err, posts){
+    try {
+      Postmodel.find({}, function(err, posts){
 
         res.render("index", {
      
@@ -53,10 +59,15 @@ const postSchema = {
           });
      
       })
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+  
 })
 app.get("/composepost",function(req,res){
-    res.render("compose",{
-       
+    res.render("compose",{   
     });
 });
 
@@ -138,7 +149,7 @@ app.get("/post/:postId", function(req, res){
       res.render("about");
   })
 
-app.listen(8000, function() {
-    console.log("Server started on port 5000");
+app.listen(8001, function() {
+    console.log("Server started on port 8001");
   });
   
